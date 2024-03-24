8b537eaa28a5d71c5c21813c4be7bda2278107d9
Gurlal Singh <gurlal.singh@paygdigitals.com>
2024-03-24 14:44:02 -0400

    first commit

---
+++ src/app/modules/auth/services/auth.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard  {
+  constructor(private authService: AuthService) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    console.log(this.authService.currentUserValue);
+    const currentUser = this.authService.currentUserValue;
+    if (currentUser) {
+      // logged in so return true
+      return true;
+    }
+
+    // not logged in so redirect to login page with the return url
+    this.authService.logout();
+    return false;
+  }
+}
